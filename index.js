e7c77003ec38ab8777dcde890c2f6f038f856a7e
Normn24 <den204d@gmail.com>
2023-04-04 19:54:09 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "6092793681:AAGuTdwlrUJGMU_L5hsB_MV-RFrH5yN_0Xc";
+const CronJob = require('cron').CronJob;
+const bot = new TelegramBot(token, { polling: true });
+
+const start = () =>{
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+
+    const job = new CronJob('0 */10 * * * *', function() {
+      return bot.sendMessage(chatId, "Test text ");
+    });
+    job.start();
+
+  });
+}
+
+start()
